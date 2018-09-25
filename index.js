30eea070722461f312f1fdb308c28b1c355dd28c
hightouch <hightouch@hotmail.fr>
2018-09-25 21:32:52 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var dsteem = require('dsteem')
 const express = require('express')
-var es = require('event-stream') 
+var es = require('event-stream')
 var util = require('util')
 const app = express()
 const port = process.env.PORT || 3000
@@ -17,64 +17,67 @@ var stream = client.blockchain.getBlockStream()
 // })).pipe(process.stdout) 
 
 stream
-.on('data', function(block) {
-    if(block.transactions[0] != undefined)
-    {
-        console.log('block ' + block.block_id)
-        var object = JSON.stringify(block.transactions)
-        object.replace('\\','')
-        object= JSON.parse(object)
-        console.log('object '+ object)
-        
-        for(i=0; i <object.length; i++)
-        {
-            console.log(object[i])
+    .on('data', function (block) {
+        if (block.transactions[0] != undefined) {
+            console.log('block ' + block.block_id)
+            var object = JSON.stringify(block.transactions)
+            object.replace('\\', '')
+            object = JSON.parse(object)
+            console.log('object ' + object)
+
+            for (i = 0; i < object.length; i++) {
+                console.log(object[i].operations)
+                if (object[i].operations[0][0] === 'transfer') {
+                    console.log('transfer')
+                    console.log(object[i].operations[0][0])
+                }
+
+            }
+
+            // if(object[0].operations)
+            // {
+            //     var type = object[0].operations[0][0]
+            //     if(type === 'transfer')
+            //     {
+            //         console.log('transfer')
+            //         console.log(object[0].operations[0][0])
+            //     }
+
+            //     if(object[1].operations[0][0])
+            //     {
+            //         var type2 = object[1].operations[0][0]
+            //         if(type2 === 'transfer')
+            //         {
+            //             console.log('transfer2')
+            //             console.log(object[1].operations[0][0])
+            //         }
+            //     }
+
+            // }
+
         }
+        // console.log('3 ' + JSON.stringify(block.transactions))
+        // console.log('4 ' + block.transactions)
 
-        // if(object[0].operations)
-        // {
-        //     var type = object[0].operations[0][0]
-        //     if(type === 'transfer')
-        //     {
-        //         console.log('transfer')
-        //         console.log(object[0].operations[0][0])
-        //     }
-    
-        //     if(object[1].operations[0][0])
-        //     {
-        //         var type2 = object[1].operations[0][0]
-        //         if(type2 === 'transfer')
-        //         {
-        //             console.log('transfer2')
-        //             console.log(object[1].operations[0][0])
-        //         }
-        //     }
- 
-        // }
-
-    }
-    // console.log('3 ' + JSON.stringify(block.transactions))
-    // console.log('4 ' + block.transactions)
-
-    //console.log(block)
-    //console.log("ID :" + block.block_id)
-    //console.log('Block : ' + JSON.parse(JSON.stringify(newblock.transactions)))
-    //console.log("Transactions :" + block['extensions'])
-    // console.log("Operations :" + block.transactions.operations)
-    // blocks.unshift(
-    //     `<div class="list-group-item"><h5 class="list-group-item-heading">Block id: ${
-    //         block.block_id
-    //     }</h5><p>Transactions in this block: ${
-    //         block.transactions.length
-    //     } <br>Witness: ${
-    //         block.witness
-    //     }</p><p class="list-group-item-text text-right text-nowrap">Timestamp: ${
-    //         block.timestamp
-    //     }</p></div>`
-    // );
-    // document.getElementById('blockList').innerHTML = blocks.join('');
-})
-.on('end', function() {
-    // done
-    console.log('END');
-});
+        //console.log(block)
+        //console.log("ID :" + block.block_id)
+        //console.log('Block : ' + JSON.parse(JSON.stringify(newblock.transactions)))
+        //console.log("Transactions :" + block['extensions'])
+        // console.log("Operations :" + block.transactions.operations)
+        // blocks.unshift(
+        //     `<div class="list-group-item"><h5 class="list-group-item-heading">Block id: ${
+        //         block.block_id
+        //     }</h5><p>Transactions in this block: ${
+        //         block.transactions.length
+        //     } <br>Witness: ${
+        //         block.witness
+        //     }</p><p class="list-group-item-text text-right text-nowrap">Timestamp: ${
+        //         block.timestamp
+        //     }</p></div>`
+        // );
+        // document.getElementById('blockList').innerHTML = blocks.join('');
+    })
+    .on('end', function () {
+        // done
+        console.log('END');
+    });
