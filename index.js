2f95b32d5681a5f1acb03c5cb83215f496109280
hightouch <hightouch@hotmail.fr>
2018-09-25 18:49:32 +0200

    update

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ console.log('listening on port 5000');
 
 var client = new dsteem.Client('https://api.steemit.com')
 
-var stream = client.blockchain.getOperations()
+var stream = client.blockchain.getBlockStream()
+
 
 stream.pipe(es.map(function(block, callback) {
-    callback(null, util.inspect(block, {colors: true, depth: null}) + '\n')
+    console.log('test' + block)
+    callback(null, util.inspect(block, {colors: true, depth: null}) + '\n'
+    )
 })).pipe(process.stdout)
