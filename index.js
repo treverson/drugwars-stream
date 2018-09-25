e806055419e99a148a2a0c3d359e24b1ed6ade89
hightouch <hightouch@hotmail.fr>
2018-09-25 17:26:40 +0200

    first commit

--- index.js
+++ index.js
@@ -1,37 +1,13 @@
-
-app.use(serve('/'));
-
-const server = app.listen(5000);
+app.listen(5000);
 
 console.log('listening on port 5000');
 
 const dsteem = require('dsteem');
 
-let opts = {};
+import {Client} from 'dsteem'
 
-//connect to production server
-opts.addressPrefix = 'STM';
-opts.chainId =
-    '0000000000000000000000000000000000000000000000000000000000000000';
+const client = new Client('https://api.steemit.com')
 
-//connect to server which is connected to the network/production
-const client = new dsteem.Client('https://api.steemit.com');
-
-let stream;
-let state;
-let blocks = [];
-//start stream
-async function main() {
-    stream = client.blockchain.getBlockStream();
-    stream
-        .on('data', function(block) {
-            //console.log(block);
-            blocks.unshift(
-                console.log(`Block id: ${block.block_id} ${block.transactions.length} ${block.witness} ${block.timestamp}`)
-                )
-                })
-        .on('end', function() {
-            // done
-            console.log('END');
-        });
+for await (const block of client.blockchain.getBlocks()) {
+    console.log(`New block, id: ${ block.block_id }`)
 }
