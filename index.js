7d546ecb7e05502a071dcade21aafc643c36122a
hightouch <hightouch@hotmail.fr>
2018-09-25 23:09:02 +0200

    update

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ con.connect(function (err) {
     if (err) throw err;
     var json = '{"skin": "none","hr_weapon": "none","hl_weapon": "none", "body": "none","bottom": "none","hat": "none"}'
     var username = "hightouch"
-    var query = "INSERT INTO users (name, inventory) VALUES ('"+username +"','"+json+"') ON DUPLICATE KEY UPDATE name=name+1"
+    var query = "INSERT INTO users (id, name, inventory) VALUES ('"+username +"','"+json+"') ON DUPLICATE KEY UPDATE name="+username+ ",inventory={'skin': 'none'}"
 
     console.log("Connected!");
     var sql = "INSERT INTO users (name, inventory) VALUES ('hightouch','[name1]')";
