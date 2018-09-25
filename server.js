08803f95eeeff9fee18fd3d5743886559cb84161
hightouch <hightouch@hotmail.fr>
2018-09-25 17:01:05 +0200

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+const express = require('express');
 
 import {Client} from 'dsteem'
-
-
+console.log('Starting App')
+const app = express();
 const server = app.listen(port, () => console.log(`Listening on ${port}`));
 
 const client = new Client('https://api.steemit.com')
