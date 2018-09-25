e0406d2251a5425457f469294f82cb0be367e602
hightouch <hightouch@hotmail.fr>
2018-09-25 18:57:56 +0200

    update

--- index.js
+++ index.js
@@ -11,7 +11,29 @@ var client = new dsteem.Client('https://api.steemit.com')
 
 var stream = client.blockchain.getBlockStream()
 
-stream.pipe(es.map(function(block, callback) {
-    console.log(block)
-    callback(null, util.inspect(block, {colors: true, depth: null}) + '\n')
-})).pipe(process.stdout) 
+// stream.pipe(es.map(function(block, callback) {
+//     console.log(block)
+//     callback(null, util.inspect(block, {colors: true, depth: null}) + '\n')
+// })).pipe(process.stdout) 
+
+
+stream
+.on('data', function(block) {
+    console.log(block);
+    // blocks.unshift(
+    //     `<div class="list-group-item"><h5 class="list-group-item-heading">Block id: ${
+    //         block.block_id
+    //     }</h5><p>Transactions in this block: ${
+    //         block.transactions.length
+    //     } <br>Witness: ${
+    //         block.witness
+    //     }</p><p class="list-group-item-text text-right text-nowrap">Timestamp: ${
+    //         block.timestamp
+    //     }</p></div>`
+    // );
+    // document.getElementById('blockList').innerHTML = blocks.join('');
+})
+.on('end', function() {
+    // done
+    console.log('END');
+});
