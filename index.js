55416c210aac76295d008f39786d60f77e65ff86
hightouch <hightouch@hotmail.fr>
2018-09-27 10:45:38 +0200

    aa

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ var mysql = require('mysql');
 const express = require('express')
 var es = require('event-stream')
 var util = require('util')
+const cors = require('cors');
+const http = require('http'),
+    https = require('https');
+http.globalAgent.maxSockets = Infinity;
+https.globalAgent.maxSockets = Infinity;
 const app = express()
 const port = process.env.PORT || 4000
+
+app.use(cors());
 app.listen(port, () => console.log(`Listening on ${port}`));
 
 var client = new dsteem.Client('https://api.steemit.com')
@@ -22,12 +29,23 @@ stream.on('data', function (block) {
         for (i = 0; i < object.length; i++) {
             var operation = object[i].operations
             if (operation[0][0] === 'transfer') {
-                console.log('block ' + block.block_id)
                 var transaction = operation[0][1]
                 if (transaction.to === "ongame") {
                     console.log('Ongame Transaction For a Character')
-                    console.log(transaction.from)
-                    Connect(transaction.from, "", "")
+                    console.log('With the block ' + block.block_id)
+                    var player = transaction.from
+                    checkForPlayer(player, function (ifExist) {
+                        if (ifExist) {
+                            StartTransaction(transaction)
+                        }
+                        else {
+                            createNewPlayer(player, function (ifExist) {
+                                if (ifExist) {
+                                    StartTransaction(transaction)
+                                }
+                            })
+                        }
+                    })
                 }
             }
             else {
@@ -42,28 +60,6 @@ stream.on('data', function (block) {
                 }
             }
         }
-
-        // if(object[0].operations)
-        // {
-        //     var type = object[0].operations[0][0]
-        //     if(type === 'transfer')
-        //     {
-        //         console.log('transfer')
-        //         console.log(object[0].operations[0][0])
-        //     }
-
-        //     if(object[1].operations[0][0])
-        //     {
-        //         var type2 = object[1].operations[0][0]
-        //         if(type2 === 'transfer')
-        //         {
-        //             console.log('transfer2')
-        //             console.log(object[1].operations[0][0])
-        //         }
-        //     }
-
-        // }
-
     }
 })
     .on('end', function () {
@@ -71,84 +67,72 @@ stream.on('data', function (block) {
         console.log('END');
     });
 
-
-// OpenConnection = function () {
-//     var con = mysql.createConnection({
-//         host: process.env.MYSQL_HOST,
-//         user: process.env.MYSQL_USERNAME,
-//         password: process.env.MYSQL_PASSWORD,
-//         database: MYSQL_DB
-//     });
-// }
-
 var pool = mysql.createPool({
     connectionLimit: 5,
-    host: "us-cdbr-iron-east-01.cleardb.net",
-    user: "bce50ec26bedce",
-    password: "13c7ceb6",
-    database: "heroku_38540d920d933f3"
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
 });
 
-Connect = function (user, transaction, action) {
+checkForPlayer = function (player, cb) {
     pool.getConnection(function (err, connection) {
-        var query = "SELECT * FROM user WHERE username='" + user + "'"
+        var query = "SELECT * FROM user WHERE username='" + player + "'"
         connection.query(query, function (err, result) {
             // Always release the connection back to the pool after the (last) query.
             if (err) throw err;
             if (result[0] != undefined) {
-                if (user = result[0].username) {
-                    console.log("User : " + user + " is already recorded");
+                if (player = result[0].username) {
+                    console.log("User : " + player + " is already recorded");
                     connection.release();
+                    cb(true)
                 }
             }
+        });
+    });
+}
 
+createNewPlayer = function (user, cb) {
+    pool.getConnection(function (err, connection) {
+        //INSERT USER
+        console.log("User : " + player + " will be recorded");
+        var player_id;
+        var query = "INSERT INTO user (username, user_type_id) VALUES ('" + player + "','1')";
+        connection.query(query, function (err, result) {
+            if (err) throw err;
             else {
-                //INSERT USER
-                console.log("User : " + user + " will be recorded");
-                var userid;
-                var query = "INSERT INTO user (username, user_type_id) VALUES ('" + user + "','1')";
+                console.log("User : " + player + " is now recorded in db")
+                //RECUPERATE USER ID
+                var query = "SELECT * FROM user WHERE username='" + player + "'"
                 connection.query(query, function (err, result) {
                     if (err) throw err;
-                    else {
-                        console.log("User : " + user + " is now recorded in db")
-
-                        //RECUPERATE USER ID
-                        var query = "SELECT * FROM user WHERE username='" + user + "'"
+                    if (result[0] != undefined) {
+                        player_id = result[0].player_id
+                        console.log("User : " + player + " will get his character and will have this id now : " + player_id);
+                        //INSERT USER CHARACTER
+                        var query = "INSERT INTO characters (character_id, character_type_id, name, alive, level, xp, money) VALUES (" + player_id + ",1,'" + player + "',1,1,1,100)"
                         connection.query(query, function (err, result) {
-                            // Always release the connection back to the pool after the (last) query.
                             if (err) throw err;
-                            if (result[0] != undefined) {
-                                user_id = result[0].user_id
-                                console.log("User : " + user + " will have this id now : " + user_id);
-                                //INSERT USER CHARACTER
-                                console.log("User : " + user + " will get his character");
-                                var query = "INSERT INTO characters (character_id, character_type_id, name, alive, level, xp, money) VALUES (" + user_id + ",1,'" + user + "',1,1,1,100)" 
+                            else {
+                                console.log("User : " + player + " have now starting values and will now get his attributes")
+                                //INSERT USER ATTRIBUTES
+                                var query = "INSERT INTO character_attribute (character_id, attribute_id, value) VALUES " + CreateAttributes(player_id);
                                 connection.query(query, function (err, result) {
                                     if (err) throw err;
                                     else {
-                                        console.log("User : " + user + " have now starting values")
-                                        //INSERT USER ATTRIBUTES
-                                        console.log("User : " + user + " will now get his attributes");
-
-                                        var query = "INSERT INTO character_attribute (character_id, attribute_id, value) VALUES " + CreateAttributes(user_id);
-                                        connection.query(query, function (err, result) {
-                                            if (err) throw err;
-                                            else {
-                                                console.log("User : " + user + " have now starting values")
-                                                connection.release();
-                                            }
-                                        })
+                                        console.log("User : " + player + " is now ready to play")
+                                        connection.release();
+                                        cb(true)
                                     }
                                 })
                             }
                         })
-
-
                     }
                 })
+
+
             }
-            // Don't use the connection here, it has been returned to the pool.
-        });
+        })
     });
 }
 
@@ -161,30 +145,8 @@ function CreateAttributes(id) {
     var query = "";
     for (i = 1; i < 11; i++) {
         query += "(" + id + "," + [i] + "," + getRandomInt(12) + ")"
-        query = query.replace(')(','),(')
+        query = query.replace(')(', '),(')
     }
-    query = query.replace(')(','),(')
+    query = query.replace(')(', '),(')
     return query
 }
-
-mysqlQuery = function (query, cb) {
-    connection.connect();
-    connection.query(query, function (error, results, fields) {
-        if (error) throw error;
-        else {
-            return cb(results)
-        }
-    });
-}
-
-
-function getHash(input) {
-    var hash = 0, len = input.length;
-    for (var i = 0; i < len; i++) {
-        hash = ((hash << 5) - hash) + input.charCodeAt(i);
-        hash |= 0; // to 32bit integer
-    }
-    return hash;
-}
-
-
