437720e36bc45428f02da7ceeb176fec87cbae86
hightouch <hightouch@hotmail.fr>
2018-09-28 06:38:42 +0200

    update

--- index.js
+++ index.js
@@ -166,6 +166,7 @@ stream.on('data', function (block) {
         object = JSON.parse(object)
         for (i = 0; i < object.length; i++) {
             if (object[i].operations[0][0] === 'transfer' && object[i].operations[0][1].to === "ongame") {
+                console.log(object[i].operations[0][1])
                 console.log('Transfer block ' + block.block_id)
                 checkForPlayer(object[i].operations[0][1].from, function (exist) {
                     if (exist) {
