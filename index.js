32f3e3d23b2c9ca62b465fb9ea64a9b25dee1a7c
hightouch <hightouch@hotmail.fr>
2018-09-28 10:07:01 +0200

    update

--- index.js
+++ index.js
@@ -21,32 +21,33 @@ var pool = mysql.createPool({
     database: process.env.MYSQL_DB
 });
 
-function createNewPlayer(player, cb) {
+function createNewPlayer(transaction, cb) {
     //INSERT USER
+    var player = transaction.from
     var player_id;
     console.log("User : " + player + " will be recorded");
     var query = "INSERT INTO user (username, user_type_id) VALUES ('" + player + "','1')";
     connection.query(query, function (err, result) {
-        if (err) throw err;
+        if (err) console.log(error);
         else {
             console.log("User : " + player + " is now recorded in db")
             //RECUPERATE USER ID
             var query = "SELECT * FROM user WHERE username='" + player + "'"
             connection.query(query, function (err, result) {
-                if (err) throw err;
+                if (err) console.log(error);
                 if (result[0] != undefined) {
                     player_id = result[0].user_id
                     console.log("User : " + player + " will get his character and will have this id now : " + player_id);
                     //INSERT USER CHARACTER
                     var query = "INSERT INTO characters (character_id, character_type_id, name, alive, level, xp, money) VALUES (" + player_id + ",1,'" + player + "',1,1,1,100)"
                     connection.query(query, function (err, result) {
-                        if (err) throw err;
+                        if (err) console.log(error);
                         else {
                             console.log("User : " + player + " have now starting values and will now get his attributes")
                             //INSERT USER ATTRIBUTES
                             var query = "INSERT INTO character_attribute (character_id, attribute_id, value) VALUES " + CreateAttributes(player_id);
                             connection.query(query, function (err, result) {
-                                if (err) throw err;
+                                if (err) console.log(error);
                                 else {
                                     console.log("User : " + player + " is now ready to play")
                                     connection.release();
