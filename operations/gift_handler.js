efa7d285dbbdee7014ce200818c681f418e17b45
hightouch <hightouch@hotmail.fr>
2018-10-05 17:30:52 +0200

    bra

--- operations/gift_handler.js
+++ operations/gift_handler.js
@@ -23,21 +23,38 @@ const gift_handler = {
                     if (result.length >= 1) {
                         //CHECK IF ITS ALREADY 6 DAY AND RESET GIFT
                         var lastday = new Date(result[0].date)
-                        var dd = lastday.getDate();
-                        var mm = lastday.getMonth() + 1; //January is 0!
-                        var yyyy = lastday.getFullYear();
-                        if (dd < 10) {
-                            dd = '0' + dd
-                        }
-                        if (mm < 10) {
-                            mm = '0' + mm
-                        }
-                        lastday = yyyy + '/' + mm + '/' + dd;
+                        var zz = lastday.getDate();
+                        var ff = lastday.getMonth() + 1; //January is 0!
+                        var tttt = lastday.getFullYear();
+
 
                         var today = new Date(date);
                         var dd = today.getDate();
                         var mm = today.getMonth() + 1; //January is 0!
                         var yyyy = today.getFullYear();
+
+                        if(zz+1 != dd){
+                            console.log("reseting days")
+                            var query = "UPDATE gift SET day=1 , date='"+today+"' WHERE username='" + user + "'"
+                            connection.query(query, function (err, result) {
+                                if (err) throw err;
+                                else {
+                                    console.log("Days reset for user" + user)
+                                    connection.release();
+                                    cb(null)
+                                }
+                            }) 
+
+                        }
+
+                        if (zz < 10) {
+                            zz = '0' + zz
+                        }
+                        if (ff < 10) {
+                            ff = '0' + ff
+                        }
+                        lastday = tttt + '/' + ff + '/' + zz;
+
                         if (dd < 10) {
                             dd = '0' + dd
                         }
@@ -45,6 +62,7 @@ const gift_handler = {
                             mm = '0' + mm
                         }
                         today = yyyy + '/' + mm + '/' + dd;
+
 
                         if(result[0].day > 6)
                         {
