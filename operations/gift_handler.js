cdb221e5b7fb1a5fe9bc1241aa65d2757f2c90f1
hightouch <hightouch@hotmail.fr>
2018-10-05 13:23:44 +0200

    update

--- operations/gift_handler.js
+++ operations/gift_handler.js
@@ -23,7 +23,7 @@ const gift_handler = {
                         //CHECK IF ITS ALREADY 6 DAY AND RESET GIFT
                         if(result[0].day = 6)
                         {
-                            var query = "UPDATE gift SET day=" + 1 + " WHERE  username=" + user;
+                            var query = "UPDATE gift SET day=" + 1 + " WHERE  username=" + user + "'";
                             connection.query(query, function (err, result) {
                                 if (err) throw err;
                                 else {
@@ -62,7 +62,7 @@ const gift_handler = {
                             cb(null)
                         }
                         else{
-                            var query = "UPDATE gift SET day=" + result[0].date+1 + " WHERE  username=" + user;
+                            var query = "UPDATE gift SET day=" + result[0].date+1 + " WHERE  username=" + user + "'";
                             connection.query(query, function (err, result) {
                                 if (err) throw err;
                                 else {
