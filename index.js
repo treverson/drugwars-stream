081ed2bbb8183c3faa503db24b1a2bbf93308fcc
hightouch <hightouch@hotmail.fr>
2018-10-24 06:39:54 +0200

    update

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ stream.on("data", function (block) {
                     if (json.json_metadata) {
                         if (json.json_metadata.tags) {
                             for (b = 0; json.json_metadata.tags.length > b; b++) {
-                                if (json.json_metadata.tags[b].includes('fundition_') || json.json_metadata.tags[b].includes('fundition-')) {
+                                if (json.json_metadata.tags[b].includes('fundition_') || json.json_metadata.tags[b].includes('fundition-') && json.root_permlink === json.permlink {
                                     var xtr = new XMLHttpRequest();
                                     xtr.open('GET', 'https://ongameapi.herokuapp.com/api/addupdate/' + json.author + "/" + json.permlink, true);
                                     xtr.send();
