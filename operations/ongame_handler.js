2aca15d89b3c193b77671118aba24d9d807bf429
hightouch <hightouch@hotmail.fr>
2018-12-05 17:29:50 +0100

    update

--- operations/ongame_handler.js
+++ operations/ongame_handler.js
@@ -31,6 +31,11 @@ const ongame_handler = {
     parseContent: function (post) {
         var content = {}
         console.log(post)
+        try {
+            content.json_metadata = JSON.parse(post.json_metadata)
+          } catch (e) {
+            console.log(e)
+          }
         if (!content) content = {}
         content.author = post.author
         content.permlink = post.permlink
