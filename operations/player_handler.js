abc7bb4b5dbf8d0df7bddb54c6275975a1888c1d
hightouch <hightouch@hotmail.fr>
2019-01-21 04:37:29 +0100

    up

--- operations/player_handler.js
+++ operations/player_handler.js
@@ -1,5 +1,11 @@
-var connection = require('../lib/dbconn');
-
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    connectionLimit: 5,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
+});
 var maxpic = 5;
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
@@ -26,48 +32,49 @@ const player_handler = {
         //INSERT USER 
         var player_id;
         console.log("User : " + player + " will be recorded");
-        var query = "INSERT INTO user (username, user_type_id) VALUES ('" + player + "','1')";
-        connection.query(query, function (err, result) {
-            if (err) console.log(error);
-            else {
-                console.log("User : " + player + " is now recorded in db")
-                //RECUPERATE USER ID
-                var query = "SELECT * FROM user WHERE username='" + player + "'"
-                connection.query(query, function (err, result) {
-                    if (err) console.log(err);
-                    if (result[0] != undefined) {
-                        player_id = result[0].user_id
-                        console.log("User : " + player + " will get his character and will have this id now : " + player_id);
-                        //INSERT USER CHARACTER
-                        var query = "INSERT INTO character (character_id, character_type_id, name, alive, level, xp, money, picture) VALUES (" + player_id + ",1,'" + player + "',1,1,1,100," + icon + ")"
-                        connection.query(query, function (err, result) {
-                            if (err) console.log(err);
-                            else {
-                                console.log("User : " + player + " have now starting values and will now get his attributes")
-                                //INSERT USER ATTRIBUTES
-                                var query = "INSERT INTO character_attribute (character_id, attribute_id, value) VALUES " + CreateAttributes(player_id);
-                                connection.query(query, function (err, result) {
-                                    if (err) console.log(err);
-                                    else {
-                                        console.log("User : " + player + " is now ready to play")
-                                        connection.end();
-                                        cb(null)
-                                    }
-                                })
-                            }
-                        })
-                    }
-                })
-            }
+        pool.getConnection(function (err, connection) {
+            var query = "INSERT INTO user (username, user_type_id) VALUES ('" + player + "','1')";
+            connection.query(query, function (err, result) {
+                if (err) console.log(error);
+                else {
+                    console.log("User : " + player + " is now recorded in db")
+                    //RECUPERATE USER ID
+                    var query = "SELECT * FROM user WHERE username='" + player + "'"
+                    connection.query(query, function (err, result) {
+                        if (err) console.log(err);
+                        if (result[0] != undefined) {
+                            player_id = result[0].user_id
+                            console.log("User : " + player + " will get his character and will have this id now : " + player_id);
+                            //INSERT USER CHARACTER
+                            var query = "INSERT INTO character (character_id, character_type_id, name, alive, level, xp, money, picture) VALUES (" + player_id + ",1,'" + player + "',1,1,1,100," + icon + ")"
+                            connection.query(query, function (err, result) {
+                                if (err) console.log(err);
+                                else {
+                                    console.log("User : " + player + " have now starting values and will now get his attributes")
+                                    //INSERT USER ATTRIBUTES
+                                    var query = "INSERT INTO character_attribute (character_id, attribute_id, value) VALUES " + CreateAttributes(player_id);
+                                    connection.query(query, function (err, result) {
+                                        if (err) console.log(err);
+                                        else {
+                                            console.log("User : " + player + " is now ready to play")
+                                            connection.release();
+                                            cb(null)
+                                        }
+                                    })
+                                }
+                            })
+                        }
+                    })
+                }
+            })
         })
     },
     checkForPlayer : function (player, cb) {
         console.log("check for player : " + player)
-            var query = "SELECT * FROM user WHERE username='" + player + "'";
+        pool.getConnection(function (err, connection) {
+            var query = "SELECT * FROM user WHERE username = '" + player + "'"
             connection.query(query, function (err, result) {
-                if (err) 
-                console.log(err)
-                cb(null);
+                if (err) throw console.log(err);
                 if (result[0] != undefined) {
                     if (player = result[0].username) {
                         console.log("User : " + player + " is already recorded");
@@ -79,8 +86,10 @@ const player_handler = {
                     cb(null)
                 }
             });
+        });
     },
     addXpToCharacter : function (character_id, xp, cb) {
+        pool.getConnection(function (err, connection) {
             var query = "SELECT * FROM character WHERE character_id = '" + character_id + "'"
             connection.query(query, function (err, result) {
                 if (err) throw err;
@@ -92,6 +101,7 @@ const player_handler = {
                         if (err) throw err;
                         else {
                             console.log(xp + "XP added to character" + character_id)
+                            connection.release();
                             cb(true)
                         }
                     })
@@ -101,6 +111,7 @@ const player_handler = {
                     cb(null)
                 }
             });
+        });
     }
 }
 module.exports = player_handler;
