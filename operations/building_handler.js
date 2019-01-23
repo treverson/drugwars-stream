fddc8bc22704fc777cba4877f0712dd23b2cc684
hightouch <hightouch@hotmail.fr>
2019-01-23 11:08:41 +0100

    a

--- operations/building_handler.js
+++ operations/building_handler.js
@@ -51,7 +51,7 @@ const building_handler = {
                                 if(cbuildings[i].production_rate > 0)
                                 var prod_rate = (building.level*cbuildings[i].production_rate)+(((cbuildings[i].production_rate*(100+building.level))/100))
                                 console.log(cost)
-                                if(type === 'drugs' && cost>player.drugs)
+                                if(Number(cost)>Number(player.drugs))
                                 {
                                     connection.release()
                                     return cb('User doesnt have enough drugs')
