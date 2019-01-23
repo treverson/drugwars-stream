c3d5988a252e3ad33c20248192822f96a9832c7d
hightouch <hightouch@hotmail.fr>
2019-01-23 13:02:50 +0100

    az

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ stream.on("data", function (block) {
                                         client.broadcast.transfer({
                                             from: 'drugwars-dealer',
                                             to: 'drugwars',
-                                            amount: amount,
+                                            amount: amount,  
                                             memo: 'Pool contribution',
                                         }, process.env.DW_DEALER_KEY)
                                     }
