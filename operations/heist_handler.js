a02541e3157ec1da50c60e98057a4fb5bf52120c
hightouch <hightouch@hotmail.fr>
2019-01-27 18:48:58 +0100

    aedf

--- operations/heist_handler.js
+++ operations/heist_handler.js
@@ -1,13 +1,12 @@
 
 var dbConnection = require('../lib/dbconn');
-var player = require('./player_handler')
 
 const heist_handler = {
     addToPool: function (player, amount, cb) {
         dbConnection.getConnection(function (err, connection) {
             var now = new Date().toISOString().slice(0, 19).replace('T', ' ')
-            var query = `INSERT INTO heist_pool (user_id, saved_drugs, date) VALUES (${player.character_id}, ${amount}, ${now})
-            ON DUPLICATE KEY UPDATE saved_drugs=+${amount}, date=${now}`
+            var query = `INSERT INTO heist_pool (user_id, saved_drugs, date) VALUES (${player.character_id}, ${amount},'${now}')
+            ON DUPLICATE KEY UPDATE saved_drugs= saved_drugs +${amount}, date='${now}'`
             connection.query(query, function (err, result) {
                 if (err) cb(err);
                 else {
