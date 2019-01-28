6e356da1b62c869f0ffa861d56d82ff231ebf004
hightouch <hightouch@hotmail.fr>
2019-01-28 08:15:36 +0100

    test

--- index.js
+++ index.js
@@ -24,19 +24,19 @@ stream.on("data", function (block) {
             console.log(error)
         }
         for (i = 0; i < object.length; i++) {
-            if (object[i].operations[0][0] === "custom_json" && object[i].operations[0][1].id === "dw-fight") {
-                try {
-                    var fight = JSON.parse(object[i].operations[0][1].json)
-                    battle.checkForABattle(fight.user_id, function (error) {
-                        if (error) {
-                            console.log(error)
-                        }
-                    })
+            // if (object[i].operations[0][0] === "custom_json" && object[i].operations[0][1].id === "dw-fight") {
+            //     try {
+            //         var fight = JSON.parse(object[i].operations[0][1].json)
+            //         battle.checkForABattle(fight.user_id, function (error) {
+            //             if (error) {
+            //                 console.log(error)
+            //             }
+            //         })
 
-                } catch (error) {
-                    console.log(error)
-                }
-            }
+            //     } catch (error) {
+            //         console.log(error)
+            //     }
+            // }
             if (object[i].operations[0][0] === "custom_json" && object[i].operations[0][1].id === "dw-char") {
                 try {
                     var json = JSON.parse(object[i].operations[0][1].json)
@@ -59,16 +59,13 @@ stream.on("data", function (block) {
                 } catch (error) {
                     console.log(error)
                 }
-                player.getPlayerId(json.username, function (user_id) {
-                    if (user_id) {
-                        player.updateGetPlayer(user_id,function(player){
-                            if(player)
-                            building.AddLevelToPlayerBuilding(player,json.building,function(result){
-                                if(result)
+
+                player.updateGetPlayer(json.username, function (player) {
+                    if (player)
+                        building.AddLevelToPlayerBuilding(player, json.building, function (result) {
+                            if (result)
                                 console.log(result)
-                            })
                         })
-                    }
                 })
             }
             if (object[i].operations[0][0] === "custom_json" && object[i].operations[0][1].id === "dw-heist") {
@@ -77,63 +74,53 @@ stream.on("data", function (block) {
                 } catch (error) {
                     console.log(error)
                 }
-                player.getPlayerId(json.username, function (user_id) {
-                    if (user_id) {
-                        player.updateGetPlayer(user_id,function(player){
-                            if(player)
-                            {
-                                heist.addToPool(player,Number(json.amount),function(result){
-                                    if(result)
-                                    console.log(result)
-                                })
-                            }
+
+                player.updateGetPlayer(json.username, function (player) {
+                    if (player) {
+                        heist.addToPool(player, Number(json.amount), function (result) {
+                            if (result)
+                                console.log(result)
                         })
                     }
                 })
             }
             if (object[i].operations[0][0] === "transfer" && object[i].operations[0][1].to === "drugwars-dealer") {
                 var json = object[i].operations[0][1]
-                player.getPlayerId(json.from, function (user_id) {
-                    if (user_id) {
-                        player.updateGetPlayer(user_id,function(player){
-                            if(player)
-                            {
-                                building_id = Number(json.memo.split(':')[1])
-                                console.log(json)
-                                if(json.memo.split(':')[0]==="upgrade")
-                                {
-                                    building.AddLevelToPlayerBuildingSteem(player,building_id,json.amount,function(result){
-                                        if(result)
-                                        console.log(result)
-                                        if(result ==="success")
-                                        {
-                                            var amount = json.amount.split(' ')[0]
-                                            amount = (amount/100)*89
-                                            amount = parseFloat(amount).toFixed(3)
-                                            const transfer = amount.concat(' ', 'STEEM');
-                                            const transf = new Object();
-                                            transf.from = 'drugwars-dealer';
-                                            transf.to = 'drugwars';
-                                            transf.amount = transfer;
-                                            transf.memo = 'Pool contribution';
-                                            client.broadcast.transfer(transf, PrivateKey.fromString(process.env.DW_DEALER_KEY)).then(
-                                                function(result) {
-                                                    console.log(
-                                                        'sent:' + transfer,
-                                                        'included in block: ' + result.block_num,
-                                                    );
-                                                },
-                                                function(error) {
-                                                    console.error(error);
-                                                }
-                                            )
+
+                player.updateGetPlayer(json.from, function (player) {
+                    if (player) {
+                        building_id = Number(json.memo.split(':')[1])
+                        console.log(json)
+                        if (json.memo.split(':')[0] === "upgrade") {
+                            building.AddLevelToPlayerBuildingSteem(player, building_id, json.amount, function (result) {
+                                if (result)
+                                    console.log(result)
+                                if (result === "success") {
+                                    var amount = json.amount.split(' ')[0]
+                                    amount = (amount / 100) * 89
+                                    amount = parseFloat(amount).toFixed(3)
+                                    const transfer = amount.concat(' ', 'STEEM');
+                                    const transf = new Object();
+                                    transf.from = 'drugwars-dealer';
+                                    transf.to = 'drugwars';
+                                    transf.amount = transfer;
+                                    transf.memo = 'Pool contribution';
+                                    client.broadcast.transfer(transf, PrivateKey.fromString(process.env.DW_DEALER_KEY)).then(
+                                        function (result) {
+                                            console.log(
+                                                'sent:' + transfer,
+                                                'included in block: ' + result.block_num,
+                                            );
+                                        },
+                                        function (error) {
+                                            console.error(error);
                                         }
-                                    })
+                                    )
                                 }
-                            }
-                           
-                        })
+                            })
+                        }
                     }
+
                 })
             }
         }
--- operations/player_handler.js
+++ operations/player_handler.js
@@ -28,57 +28,13 @@ createUniqueId = function () {
 
 
 const player_handler = {
-    createNewPlayer: function (player, icon, referrer, cb) {
-        //INSERT USER 
-        var player_id;
-        console.log("User : " + player + " will be recorded");
+    checkPlayer: function (player, cb) {
         pool.getConnection(function (err, connection) {
-            var query = "INSERT INTO user (username, user_type_id) VALUES ('" + player + "','1')";
-            connection.query(query, function (err, result) {
-                if (err) console.log(error);
-                else {
-                    console.log("User : " + player + " is now recorded in db")
-                    //RECUPERATE USER ID
-                    var query = "SELECT * FROM user WHERE username='" + player + "'"
-                    connection.query(query, function (err, result) {
-                        if (err) console.log(err);
-                        if (result[0] != undefined) {
-                            player_id = result[0].user_id
-                            console.log("User : " + player + " will get his character and will have this id now : " + player_id);
-                            //INSERT USER CHARACTER
-                            var query = "INSERT INTO `character` (character_id, character_type_id, name, alive, level, xp, money, picture, drugs, weapon_production_rate, last_update, drug_production_rate, weapons,rewards, referrer ) VALUES ('" + player_id + "', 1,'" + player + "', 1, 1, 1, 100,'" + icon + "', 1000, 0.10,'" + new Date().toISOString().slice(0, 19).replace('T', ' ') + "',0.10,1000,0,'" + referrer + "')"
-                            connection.query(query, function (err, result) {
-                                if (err) console.log(err);
-                                else {
-                                    console.log("User : " + player + " have now starting values and will now get his attributes")
-                                    //INSERT USER BUILDINGS
-                                    var query = "INSERT INTO character_buildings (character_id) VALUES (" + player_id + ")"
-                                    connection.query(query, function (err, result) {
-                                        if (err) console.log(err);
-                                        else {
-                                            console.log("User : " + player + " is now ready to play")
-                                            connection.release();
-                                            cb(null)
-                                        }
-                                    })
-
-                                }
-                            })
-                        }
-                    })
-                }
-            })
-        })
-    },
-    getPlayerId: function (player, cb) {
-        pool.getConnection(function (err, connection) {
-            var query = "SELECT * FROM user WHERE username = '" + player + "'"
+            var query = "SELECT * FROM `character` WHERE name = '" + player + "'"
             connection.query(query, function (err, result) {
                 if (err) throw console.log(err);
                 if (result[0] != undefined) {
-                    if (player = result[0].username) {
                         cb(result[0].user_id)
-                    }
                 }
                 else {
                     console.log("User : " + player + " isnt recorded");
@@ -87,19 +43,45 @@ const player_handler = {
             });
         });
     },
-    addXpToCharacter: function (character_id, xp, cb) {
+    createNewPlayer: function (player, icon, referrer, cb) {
+        //INSERT USER 
         pool.getConnection(function (err, connection) {
-            var query = "SELECT * FROM character WHERE character_id = '" + character_id + "'"
+            console.log("User : " + player + " will get his character and will have this id now : ");
+            //INSERT USER CHARACTER
+            var query = "INSERT INTO `character` (character_type_id, name, alive, level, xp, money, picture, drugs, weapon_production_rate, last_update, drug_production_rate, weapons,rewards, referrer ) VALUES (1,'" + player + "', 1, 1, 1, 100,'" + icon + "', 1000, 0.10,'" + new Date().toISOString().slice(0, 19).replace('T', ' ') + "',0.10,1000,0,'" + referrer + "')"
+            connection.query(query, function (err, result) {
+                if (err) console.log(err);
+                else {
+                    console.log("User : " + player + " have now starting values and will now get his attributes")
+                    //INSERT USER BUILDINGS
+                    var query = "INSERT INTO character_buildings (name) VALUES (" + player_id + ")"
+                    connection.query(query, function (err, result) {
+                        if (err) console.log(err);
+                        else {
+                            console.log("User : " + player + " is now ready to play")
+                            connection.release();
+                            cb(null)
+                        }
+                    })
+
+                }
+            })
+
+        })
+    },
+    addXpToCharacter: function (name, xp, cb) {
+        pool.getConnection(function (err, connection) {
+            var query = "SELECT * FROM character WHERE name = '" + name + "'"
             connection.query(query, function (err, result) {
                 if (err) throw err;
                 if (result[0] != undefined) {
-                    console.log(xp + "XP will be add to " + character_id)
+                    console.log(xp + "XP will be add to " + name)
                     var character_new_xp = result[0].xp + xp
-                    var query = "UPDATE character SET xp=" + character_new_xp + " WHERE  character_id=" + character_id;
+                    var query = "UPDATE character SET xp=" + character_new_xp + " WHERE  name=" + name;
                     connection.query(query, function (err, result) {
                         if (err) throw err;
                         else {
-                            console.log(xp + "XP added to character" + character_id)
+                            console.log(xp + "XP added to character" + name)
                             connection.release();
                             cb(true)
                         }
@@ -112,23 +94,9 @@ const player_handler = {
             });
         });
     },
-    addLevelToPlayerBuilding: function (character_id, building_id, cb) {
+    updateGetPlayer: function (name, cb) {
         pool.getConnection(function (err, connection) {
-            var now = new Date().toISOString().slice(0, 19).replace('T', ' ')
-            var query = `UPDATE character_buildings SET building_${building_id}_level=+1, building_${building_id}_last_update='${now}'  WHERE character_id=${character_id}`
-            connection.query(query, function (err, result) {
-                if (err) cb(err);
-                else {
-                    console.log("Upgraded character building :" + building_id + " for : " + character_id)
-                    connection.release();
-                    cb(null)
-                }
-            })
-        })
-    },
-    updateGetPlayer: function (character_id, cb) {
-        pool.getConnection(function (err, connection) {
-            var query = "SELECT * FROM `character` WHERE character_id=" + character_id
+            var query = "SELECT * FROM `character` WHERE name =" + name
             connection.query(query, function (err, result) {
                 if (err) console.log(err);
                 if (result) {
@@ -138,7 +106,7 @@ const player_handler = {
                     var differenceprod = now.getTime() - player.last_update.getTime()
                     var drug_balance = player.drugs + Number(parseFloat((differenceprod / 1000) * player.drug_production_rate).toFixed(2))
                     var weapon_balance = player.weapons + Number(parseFloat((differenceprod / 1000) * player.weapon_production_rate).toFixed(0))
-                    var query = `UPDATE \`character\` SET drugs=${drug_balance}, weapons=${weapon_balance}, last_update='${nowtomysql}' WHERE  character_id=${character_id}`
+                    var query = `UPDATE \`character\` SET drugs=${drug_balance}, weapons=${weapon_balance}, last_update='${nowtomysql}' WHERE  name=${name}`
                     connection.query(query, function (err, result) {
                         if (err) throw err;
                         else {
@@ -153,9 +121,9 @@ const player_handler = {
             });
         });
     },
-    updateProductionRate: function (character_id, type, rate, cb) {
+    updateProductionRate: function (name, type, rate, cb) {
         pool.getConnection(function (err, connection) {
-            var query = `UPDATE \`character\` SET ${type}=+${rate} WHERE  character_id=${character_id}`
+            var query = `UPDATE \`character\` SET ${type}=+${rate} WHERE  name=${name}`
             connection.query(query, function (err, result) {
                 if (err) throw err;
                 else {
@@ -166,19 +134,19 @@ const player_handler = {
             })
         });
     },
-    removeDrugs: function (character_id, building_id, cb) {
+    removeDrugs: function (name, building_id, cb) {
         pool.getConnection(function (err, connection) {
-            var query = "SELECT * FROM `character` WHERE character_id = '" + character_id + "'"
+            var query = "SELECT * FROM `character` WHERE name = '" + name + "'"
             connection.query(query, function (err, result) {
                 if (err) throw err;
                 if (result[0] != undefined) {
-                    console.log(xp + "XP will be add to " + character_id)
+                    console.log(xp + "XP will be add to " + name)
                     var character_new_xp = result[0].xp + xp
-                    var query = "UPDATE character SET xp=" + character_new_xp + " WHERE  character_id=" + character_id;
+                    var query = "UPDATE character SET xp=" + character_new_xp + " WHERE  name=" + name;
                     connection.query(query, function (err, result) {
                         if (err) throw err;
                         else {
-                            console.log(xp + "XP added to character" + character_id)
+                            console.log(xp + "XP added to character" + name)
                             connection.release();
                             cb(true)
                         }
