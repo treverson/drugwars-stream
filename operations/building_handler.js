b217f8ba05140001b48b4a51437c512d654c7764
hightouch <hightouch@hotmail.fr>
2019-01-28 02:53:56 +0100

    test

--- operations/building_handler.js
+++ operations/building_handler.js
@@ -48,7 +48,7 @@ const building_handler = {
                                 {
                                     if(building.level === 0)
                                     {
-                                        timer =  (15 - (cbuildings.buildings.building_1_level/100)) * (1 * cbuildings[i].building_coeff)
+                                        timer =  (15 - (building.level/100)) * (1 * cbuildings[i].building_coeff)
                                         var z = (1) * cbuildings[i].building_base_price
                                         cost = (z*((1)*cbuildings[i].building_coeff))
                                         old_prod_rate = 0
@@ -56,7 +56,7 @@ const building_handler = {
                                     }
                                    
                                     else{
-                                        timer =  (15 - (cbuildings.buildings.building_1_level/100)) * ((building.level+1) * cbuildings[i].building_coeff)
+                                        timer =  (15 - (building.level/100)) * ((building.level+1) * cbuildings[i].building_coeff)
                                         var z = (building.level+1) * cbuildings[i].building_base_price
                                         cost = (z*((building.level+1)*cbuildings[i].building_coeff))
                                         prod_rate = (((building.level+1))*cbuildings[i].production_rate)+(((cbuildings[i].production_rate*(100+((building.level+1))))/100))
@@ -67,13 +67,13 @@ const building_handler = {
                                 else{
                                     if(building.level === 0)
                                     {
-                                        timer = (15 - (cbuildings.buildings.building_1_level/100)) * (1 * cbuildings[i].building_coeff)
+                                        timer = (15 - (building.level/100)) * (1 * cbuildings[i].building_coeff)
                                         var z = (1) * cbuildings[i].building_base_price
                                         cost = (z*((1)*cbuildings[i].building_coeff))
                                     }
                                    
                                     else{
-                                        timer =  (15 - (cbuildings.buildings.building_1_level/100)) * ((building.level+1) * cbuildings[i].building_coeff)
+                                        timer =  (15 - (building.level/100)) * ((building.level+1) * cbuildings[i].building_coeff)
                                         var z = (building.level+1) * cbuildings[i].building_base_price
                                         cost = (z*((building.level+1)*cbuildings[i].building_coeff))
                                     }
