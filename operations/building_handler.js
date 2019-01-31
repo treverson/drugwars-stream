7e70c030bc7a7f6a147f5fc122e5bfd0dd346e5c
hightouch <hightouch@hotmail.fr>
2019-01-31 23:12:03 +0100

    test

--- operations/building_handler.js
+++ operations/building_handler.js
@@ -27,8 +27,6 @@ const building_handler = {
                     building_last_update = now
                 }
                 //CHECK LAST UPDATE
-                var now = new Date();
-                console.log(building_last_update,now)
                 if (building_last_update <= now) {
                     var timer = building_handler.calculateTime(hq_level, building_level, current_building)
                     console.log(timer)
