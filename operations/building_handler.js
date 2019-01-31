24f5e97cdf6d15b015ca1f7fc58978c4491cdfff
hightouch <hightouch@hotmail.fr>
2019-01-31 21:00:09 +0100

    test

--- operations/building_handler.js
+++ operations/building_handler.js
@@ -33,52 +33,46 @@ const building_handler = {
                 console.log(timer)
                 var cost = building_handler.calculateCost(building_level, current_building)
                 console.log(cost)
-
+                if (cost > character.drugs) {
+                    return cb('User doesnt have enough drugs')
+                }
                 if (current_building.production_rate > 0) {
                     var prod_rate = current_building.production_rate
                     var prod_type = current_building.production_type
                 }
 
-                if (cost > player.drugs) {
-                    return cb('User doesnt have enough drugs')
+
+
+                console.log('next update' + building.last_update)
+                var query;
+                if (ptype === 'weapons') {
+                    if (prod_rate)
+                        character.weapon_production_rate = (character.weapon_production_rate - old_prod_rate) + prod_rate
+                    character.drugs = character.drugs - cost
+                    query = "UPDATE `character` SET weapon_production_rate=" + character.weapon_production_rate + ", drugs=" + character.drugs + " WHERE name='" + character.name + "'"
                 }
                 else {
-                    var d = new Date();
-                    if (building.last_update < d) {
-                        console.log('next update' + building.last_update)
-                        var nowtomysql = new Date().toISOString().slice(0, 19).replace('T', ' ')
-                        var query;
-                        if (ptype === 'weapons') {
-                            if (prod_rate)
-                                player.weapon_production_rate = (player.weapon_production_rate - old_prod_rate) + prod_rate
-                            player.drugs = player.drugs - cost
-                            query = "UPDATE `character` SET weapon_production_rate=" + player.weapon_production_rate + ", drugs=" + player.drugs + " WHERE name='" + player.name + "'"
-                        }
-                        else {
-                            if (prod_rate)
-                                player.drug_production_rate = (player.drug_production_rate - old_prod_rate) + prod_rate
-                            player.drugs = player.drugs - cost
-                            query = "UPDATE `character` SET drug_production_rate=" + player.drug_production_rate + ", drugs=" + player.drugs + "  WHERE name='" + player.name + "'"
-                        }
+                    if (prod_rate)
+                        character.drug_production_rate = (character.drug_production_rate - old_prod_rate) + prod_rate
+                    character.drugs = character.drugs - cost
+                    query = "UPDATE `character` SET drug_production_rate=" + character.drug_production_rate + ", drugs=" + character.drugs + "  WHERE name='" + character.name + "'"
+                }
+                db.query(query, function (err, result) {
+                    if (err) throw err;
+                    else {
+                        var now = new Date(now.getTime() + (timer * 1000)).toISOString().slice(0, 19).replace('T', ' ')
+                        var query = `UPDATE character_buildings SET building_${building_id}_level=${Number(building.level + 1)}, building_${building_id}_last_update='${now}'  WHERE name='${character.name}'`
                         db.query(query, function (err, result) {
-                            if (err) throw err;
+                            if (err) cb(err);
                             else {
-                                var now = new Date(d.getTime() + (timer * 1000)).toISOString().slice(0, 19).replace('T', ' ')
-                                var query = `UPDATE character_buildings SET building_${building_id}_level=${Number(building.level + 1)}, building_${building_id}_last_update='${now}'  WHERE name='${player.name}'`
-                                db.query(query, function (err, result) {
-                                    if (err) cb(err);
-                                    else {
-                                        console.log("Upgraded character building :" + building_id + " for : " + player.name)
-                                        cb('success')
-                                    }
-                                })
+                                console.log("Upgraded character building :" + building_id + " for : " + character.name)
+                                cb('success')
                             }
                         })
                     }
-                    else {
-                        cb('need to wait')
-                    }
-                }
+                })
+
+
 
 
             }
