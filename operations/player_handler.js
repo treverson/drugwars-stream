a17d0cf03e36a51bb7e58afed12178805d8ce9cc
hightouch <hightouch@hotmail.fr>
2019-02-01 14:00:35 +0100

    added bonus operation center at update

--- operations/player_handler.js
+++ operations/player_handler.js
@@ -40,31 +40,73 @@ const player_handler = {
             }
         })
     },
-    getUpdateCharacter: function (name, cb) {
-        var query = "SELECT * FROM `character` WHERE name ='" + name + "'"
-        db.query(query, function (err, result) {
-            if (err || !result || !result[0] || result[0] === null){
+    getUpdateCharacter: function (username, cb) {
+        let query = "SELECT * FROM `character` WHERE name = ?";
+        db.query(query, [username], function (err, result) {
+            if (err || !result || !result[0] || result[0] === null) {
                 console.log(err);
                 cb(null)
-            } 
-            else{
-                player = result[0]
-                var now = new Date()
-                var nowtomysql = new Date().toISOString().slice(0, 19).replace('T', ' ')
-                var differenceprod = now.getTime() - player.last_update.getTime()
-                var drug_balance = player.drugs + Number(parseFloat((differenceprod / 1000) * player.drug_production_rate).toFixed(2))
-                var weapon_balance = player.weapons + Number(parseFloat((differenceprod / 1000) * player.weapon_production_rate).toFixed(0))
-                var query = `UPDATE \`character\` SET drugs=${drug_balance}, weapons=${weapon_balance}, last_update='${nowtomysql}' WHERE  name='${name}'`
-                db.query(query, function (err, result) {
-                    if (err) throw err;
-                    else {
-                        player.drugs = drug_balance
-                        player.weapons = weapon_balance
-                        console.log("Player - Updated character " + player.name + ' new drug balance : ' + drug_balance + 'new weapon balance : ' + weapon_balance)
-                        cb(player)
-                    }
-                })
             }
+            else {
+                const character = result[0]
+                query = "SELECT * FROM character_buildings WHERE name = ?";
+                  db.query(query,[character.name],
+                      function (err, result) {
+                          if (err) {
+                              console.log(err);
+                              cb(null);
+                          } else {
+                              const [[buildings], [heist]] = result;
+                              var now = new Date()
+                              var nowtomysql = new Date().toISOString().slice(0, 19).replace('T', ' ')
+                              var differenceprod = now.getTime() - character.last_update.getTime()
+                              var drug_balance = character.drugs + Number(parseFloat((differenceprod / 1000) * character.drug_production_rate).toFixed(2))
+                              var weapon_balance = character.weapons + Number(parseFloat((differenceprod / 1000) * character.weapon_production_rate).toFixed(0))
+                              console.log(buildings.building_4_level)
+                              if(buildings.building_4_level > 0)
+                              {
+                                drug_balance = drug_balance + (drug_balance*(buildings.building_4_level/200))
+                                weapon_balance = weapon_balance + (weapon_balance*(buildings.building_4_level/200))
+                              }
+                              var query = `UPDATE \`character\` SET drugs=${drug_balance}, weapons=${weapon_balance}, last_update='${nowtomysql}' WHERE  name='${name}'`
+                              db.query(query, function (err, result) {
+                                  if (err) throw err;
+                                  else {
+                                      character.drugs = drug_balance
+                                      character.weapons = weapon_balance
+                                      console.log("character - Updated character " + character.name + ' new drug balance : ' + drug_balance + 'new weapon balance : ' + weapon_balance)
+                                      cb(player)
+                                  }
+                              })
+                          }
+                      }
+                  );
+
+               
+            }
+        });
+    },
+    getCharacter: function (username, cb) {
+        let query = "SELECT * FROM `character` WHERE name = ?";
+        db.query(query, [username], function (err, result) {
+            if (err || !result || !result[0])
+                return cb(null);
+            const character = result[0];
+            query = "SELECT * FROM character_buildings WHERE name = ?; \n\
+          SELECT * FROM heist_pool WHERE name = ?";
+            db.query(
+                query,
+                [character.name, character.name],
+                function (err, result) {
+                    if (err) {
+                        console.log(err);
+                        cb(null);
+                    } else {
+                        const [[buildings], [heist]] = result;
+                        cb({ character, buildings, heist });
+                    }
+                }
+            );
         });
     },
     updateProductionRate: function (name, type, rate, cb) {
@@ -81,7 +123,7 @@ const player_handler = {
         })
     },
     removeDrugs: function (name, amount, cb) {
-        var query = "UPDATE `character` SET drugs=-"+amount+" WHERE name='"+name +"'"
+        var query = "UPDATE `character` SET drugs=-" + amount + " WHERE name='" + name + "'"
         db.query(query, function (err, result) {
             if (err) {
                 console.log(err)
