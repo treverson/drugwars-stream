34a35ccad8f65b28261b870c0e10b94385fd5c4a
hightouch <hightouch@hotmail.fr>
2019-02-03 22:55:13 +0100

    bra

--- operations/building_handler.js
+++ operations/building_handler.js
@@ -10,9 +10,9 @@ for (i = 0; i < gamebase.buildings.length; i++) {
 }
 
 const building_handler = {
-    updateBuilding: function (character, building_id, amount, cb) {
+    updateBuilding: function (user, building_id, amount, cb) {
         var query = "SELECT * FROM buildings WHERE username = ?"
-        db.query(query, [character.username], function (err, character_buildings) {
+        db.query(query, [user.username], function (err, character_buildings) {
             if (err) {
                 console.log(err)
                 cb(null)
@@ -21,37 +21,33 @@ const building_handler = {
                 var now = new Date();
                 var character_buildings = JSON.parse(JSON.stringify(character_buildings))
                 var building_placeholder = buildings.filter(function (item) { return item.id === building_id; })[0]
-
-                var hq_level = character_buildings.filter(function (item) { return item.name === "headquarters"; });
-                var crack = character_buildings.filter(function (item) { return item.name === "crackhouse"; });
+                var headquarters = character_buildings.filter(function (item) { return item.name === "headquarters"})[0]
                 if(character_buildings.filter(function (item) { return item.name === building_id; }))
-                var current_building = character_buildings.filter(function (item) { return item.name === building_id; })
+                var building_level = character_buildings.filter(function (item) { return item.name === building_id})[0].level
                 else{
-                    current_building = "brraa"
+                    var building_level = 0
                 }
-
-                console.log(building_placeholder,current_building,hq_level,crack[0].name)
-                var building_level = building_placeholder.level + 1
+                building_level+=1
                 //CHECK HEADQUARTER LEVEL
-                if (hq_level < building_placeholder.level && building_id != "headquarters") {
+                if (headquarters.level < building_level && building_id != headquarters.name) {
                     return cb('hq level to low')
                 }
-                if (building_placeholder.next_update != null)
-                    var next_update = building_placeholder.next_update
+                if (building_level.next_update != null)
+                    var next_update = building_level.next_update
                 else {
-                    next_update = now
+                    var next_update = now
                 }
                 //CHECK LAST UPDATE
                 if (next_update <= now) {
-                    var timer = building_handler.calculateTime(hq_level, building_level, building_placeholder)
+                    var timer = building_handler.calculateTime(headquarters.level, building_level, building_placeholder)
                     console.log(timer)
-                    var cost = building_handler.calculateCost(building_level, building_placeholder)
+                    var cost = building_handler.calculateCost(building_level.level, building_placeholder)
                     //CHECK DRUGS COST BALANCE
-                    if (cost > character.drugs && !amount) {
+                    if (cost > user.drugs && !amount) {
                         return cb('not enough drugs')
                     }
-                    if (cost < character.drugs && !amount) {
-                        building_handler.confirmBuildingUpdate(character, now, building_level, building_id, timer, building_placeholder, cost, function (result) {
+                    if (cost < user.drugs && !amount) {
+                        building_handler.confirmBuildingUpdate(user.username, now, building_level, building_id, timer, building_placeholder, cost, function (result) {
                             if (result)
                             return cb(result)
                         })
@@ -64,7 +60,7 @@ const building_handler = {
                                 {
                                     cost = 0
                                     timer = 1
-                                    building_handler.confirmBuildingUpdate(character, now, building_level, building_id, timer, building_placeholder, cost, function (result) {
+                                    building_handler.confirmBuildingUpdate(user.username, now, building_level, building_id, timer, building_placeholder, cost, function (result) {
                                         if (result)
                                         return cb(result)
                                     })
@@ -87,12 +83,6 @@ const building_handler = {
     calculateCost: function (building_level, building_placeholder) {
         return (building_placeholder.base_price * (building_level * building_placeholder.coeff))
     },
-    calculateProductionRate: function (building_level, building_placeholder) {
-        return (building_placeholder.production_rate * (building_level * building_placeholder.coeff))
-    },
-    calculateAttack: function (building_level, building_placeholder) {
-        return (building_placeholder.production_rate * (building_level * building_placeholder.coeff))
-    },
     confirmBuildingUpdate: function (character, now, building_level, building_id, timer, building_placeholder, cost, cb) {
         var query;
         var next_update_time = new Date(now.getTime() + (timer * 1000)).toISOString().slice(0, 19).replace('T', ' ')
@@ -100,22 +90,25 @@ const building_handler = {
             var old_rate = building_handler.calculateProductionRate(building_level - 1, building_placeholder)
             var production_rate = building_handler.calculateProductionRate(building_level, building_placeholder)
             if (building_placeholder.production_type === 'weapon') {
-                character.weapon_production_rate = (character.weapon_production_rate - old_rate) + production_rate
-                character.drugs = character.drugs - cost
-                query = "UPDATE `character` SET weapon_production_rate=" + character.weapon_production_rate + ", drugs=" + character.drugs + " WHERE name='" + character + "'; \n\
-                UPDATE character_buildings SET building_"+ building_id + "_level=" + building_level + ", building_" + building_id + "_last_update='" + next_update_time + "'  WHERE name='" + character + "'";
+                user.weapon_production_rate = (user.weapon_production_rate - old_rate) + production_rate
+                user.drugs = user.drugs - cost
+                query = `UPDATE users SET weapon_production_rate=${user.weapon_production_rate}, drugs=${user.drugs} WHERE username='${user.username}'; \n\
+                INSERT INTO buildings (username , name, level, next_update) VALUES ('${user.username}','${building_placeholder.name}', ${building_level},'${next_update_time}'); \n\
+                ON DUPLICATE KEY UPDATE level=${building_level}, next_update='${next_update_time}'`
             }
             else {
-                character.drug_production_rate = (character.drug_production_rate - old_rate) + production_rate
-                character.drugs = character.drugs - cost
-                query = "UPDATE `character` SET drug_production_rate=" + character.drug_production_rate + ", drugs=" + character.drugs + "  WHERE name='" + character + "'; \n\
-                UPDATE character_buildings SET building_"+ building_id + "_level=" + building_level + ", building_" + building_id + "_last_update='" + next_update_time + "'  WHERE name='" + character + "'";
+                user.drug_production_rate = (user.drug_production_rate - old_rate) + production_rate
+                user.drugs = user.drugs - cost
+                query = `UPDATE users SET drug_production_rate=${user.drug_production_rate}, drugs=${user.drugs} WHERE username='${user.username}'; \n\
+                INSERT INTO buildings (username , name, level, next_update) VALUES ('${user.username}','${building_placeholder.name}', ${building_level},'${next_update_time}'); \n\
+                ON DUPLICATE KEY UPDATE level=${building_level}, next_update='${next_update_time}'`
             }
         }
         else {
-            character.drugs = character.drugs - cost
-            query = "UPDATE `character` SET drugs=" + character.drugs + "  WHERE name='" + character + "'; \n\
-            UPDATE character_buildings SET building_"+ building_id + "_level=" + building_level + ", building_" + building_id + "_last_update='" + next_update_time + "'  WHERE name='" + character + "'";
+            user.drugs = user.drugs - cost
+            query = `UPDATE users SET drugs=${user.drugs} WHERE username='${user.username}'; \n\
+            INSERT INTO buildings (username , name, level, next_update) VALUES ('${user.username}','${building_placeholder.name}', ${building_level},'${next_update_time}'); \n\
+            ON DUPLICATE KEY UPDATE level=${building_level}, next_update='${next_update_time}'`
         }
         db.query(query, function (err, result) {
             if (err) {
@@ -123,7 +116,7 @@ const building_handler = {
                 cb(err);
             }
             else {
-                console.log("Upgraded character building :" + building_id + " for : " + character)
+                console.log("Upgraded character building :" + building_id + " for : " + user.username)
                 cb('success')
             }
         })
