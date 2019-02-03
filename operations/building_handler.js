bf2e6c18cbb9538769a9021944c854b0cf63197e
hightouch <hightouch@hotmail.fr>
2019-02-03 23:16:45 +0100

    bra

--- operations/building_handler.js
+++ operations/building_handler.js
@@ -97,23 +97,23 @@ const building_handler = {
             if (building_placeholder.production_type === 'weapon') {
                 user.weapon_production_rate = (user.weapon_production_rate - old_rate) + production_rate
                 user.drugs = user.drugs - cost
-                query = `UPDATE users SET weapon_production_rate=${user.weapon_production_rate}, drugs=${user.drugs} WHERE username='${user.username}'; \n\
-                INSERT INTO buildings (username , name, level, next_update) VALUES ('${user.username}','${building_placeholder.name}', ${building_level},'${next_update_time}') \n\
+                query = `UPDATE users SET weapon_production_rate=${user.weapon_production_rate}, drugs=${user.drugs} WHERE username='${user.username}';
+                INSERT INTO buildings (username , name, level, next_update) VALUES ('${user.username}','${building_placeholder.name}', ${building_level},'${next_update_time}') 
                 ON DUPLICATE KEY UPDATE level=${building_level}, next_update='${next_update_time}'`
             }
             else {
                 user.drug_production_rate = (user.drug_production_rate - old_rate) + production_rate
                 user.drugs = user.drugs - cost
-                query = `UPDATE users SET drug_production_rate=${user.drug_production_rate}, drugs=${user.drugs} WHERE username='${user.username}'; \n\
-                INSERT INTO buildings (username , name, level, next_update) VALUES ('${user.username}','${building_placeholder.name}', ${building_level},'${next_update_time}') \n\
+                query = `UPDATE users SET drug_production_rate=${user.drug_production_rate}, drugs=${user.drugs} WHERE username='${user.username}';
+                INSERT INTO buildings (username , name, level, next_update) VALUES ('${user.username}','${building_placeholder.name}', ${building_level},'${next_update_time}') 
                 ON DUPLICATE KEY UPDATE level=${building_level}, next_update='${next_update_time}'`
             }
         }
         //IF DOESNT PRODUCE ANYTHING
         else {
             user.drugs = user.drugs - cost
-            query = `UPDATE users SET drugs=${user.drugs} WHERE username='${user.username}'; \n\
-            INSERT INTO buildings (username , name, level, next_update) VALUES ('${user.username}','${building_placeholder.name}', ${building_level},'${next_update_time}') \n\
+            query = `UPDATE users SET drugs=${user.drugs} WHERE username='${user.username}';
+            INSERT INTO buildings (username , name, level, next_update) VALUES ('${user.username}','${building_placeholder.name}', ${building_level},'${next_update_time}')
             ON DUPLICATE KEY UPDATE level=${building_level}, next_update='${next_update_time}'`
         }
         db.query(query, function (err, result) {
