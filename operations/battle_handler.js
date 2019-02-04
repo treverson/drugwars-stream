4037fba649df630eec86558644315a2d294a30f1
hightouch <hightouch@hotmail.fr>
2019-02-04 21:26:31 +0100

    bra

--- operations/battle_handler.js
+++ operations/battle_handler.js
@@ -8,12 +8,12 @@ const battle_handler = {
         var next_update_time = new Date(now.getTime() + (timer * 1000)).toISOString().slice(0, 19).replace('T', ' ')
         var target_block = block_num + (timer/3)
         query.push(`INSERT INTO battles (username, defender, next_update, battle_key, target_block) 
-                    VALUES ('${username}','${defender}','${next_update_time}',${key},${target_block})`)
+                    VALUES ('${username}','${defender}','${next_update_time}','${key}',${target_block})`)
         for(i=0;i<army.length;i++)
         {
             query.push(`UPDATE users_units SET amount=amount-${army[i].amount} WHERE unit='${army[i].unit}' AND username='${username}'`)
             query.push(`INSERT INTO battles_units (username, unit, amount, battle_key) 
-                        VALUES ('${username}','${army[i].unit}',${army[i].amount},${key})`)
+                        VALUES ('${username}','${army[i].unit}',${army[i].amount},'${key}')`)
         }
         query = query.join(';')
         db.query(query, [username], function (err, result) {
