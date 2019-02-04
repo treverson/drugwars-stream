ae613a755497917f0faa0e03864bcccf9bb9853a
hightouch <hightouch@hotmail.fr>
2019-02-04 20:05:47 +0100

    bra

--- operations/battle_handler.js
+++ operations/battle_handler.js
@@ -1,32 +1,9 @@
 const battle_handler = {
     checkForABattle: function (player_id,cb) {
-        if (battle_id > 0) {
-            JoinBattle(player_id, battle_id, function (error) {
-                if (error)
-                    console.log(error)
-            })
-        }
+  
     },
     JoinBattle:function (player_id, battle_id, cb) {
-        pool.getConnection(function (err, connection) {
-            var query = "UPDATE battle SET battle_player_two_id=" + player_id + " WHERE battle_id=" + battle_id
-            connection.query(query, function (err, result) {
-                if (err) cb(true);
-                else {
-                    console.log("User : " + player_id + " joined battle " + battle_id)
-                    ResolveBattle(battle_id, function (error) {
-                        if (error)
-                            console.log(error)
-                        else {
-                            connection.release();
-                            console.log("Battle " + battle_id + " solved")
-                            cb(null)
-                        }
-                    })
     
-                }
-            })
-        })
     }
 }
 
