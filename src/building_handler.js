93a59ab01a7b3581bb3f2a7010c9a30e3d87e6d6
hightouch <hightouch@hotmail.fr>
2019-02-07 15:39:02 +0100

    bra

--- src/building_handler.js
+++ src/building_handler.js
@@ -42,8 +42,7 @@ const building_handler = {
         building_level += 1;
         // CHECK HEADQUARTER LEVEL
         var headquarters = character_buildings.filter(item => item.building === 'headquarters' )
-        console.log(headquarters)
-        headquarters.lvl = headquarters.lvl
+        headquarters.lvl = headquarters[0].lvl
         if (headquarters.lvl < building_level && building_name != 'headquarters') {
           return cb('hq level to low');
         }
