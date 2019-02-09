0999423ae2464212777423c3132920b484564f08
adcpm <fabien@bonustrack.co>
2019-02-09 19:32:01 +0700

    Use fetch instead of xmlhttprequest

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,27 +1,19 @@
-const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const fetch = require('node-fetch');
 
-const utils = {
-  costToSteem(int, cb) {
-    const xtr = new XMLHttpRequest();
-    xtr.open('GET', 'https://api.coinmarketcap.com/v1/ticker/steem/', true);
-    xtr.send();
-    xtr.onreadystatechange = function() {
-      if (xtr.readyState == 4) {
-        if (xtr.status == 200) {
-          if (xtr.responseText) {
-            try {
-              var ticker = JSON.parse(xtr.responseText);
-            } catch (e) {}
-            const price = parseFloat(int / 10000 / ticker[0].price_usd).toFixed(3);
-            cb(price);
-          }
-        } else {
-          console.log('Error: API not responding!');
-          cb(null);
-        }
-      }
-    };
-  },
+function costToSteem(int, cb) {
+  fetch('https://api.coinmarketcap.com/v1/ticker/steem/')
+    .then(res =>
+      res.json().then(result => {
+        const price = parseFloat(int / 10000 / result[0].price_usd).toFixed(3);
+        cb(price);
+      }),
+    )
+    .catch(err => {
+      console.log('Error: API not responding!', err);
+      cb(null);
+    });
+}
+
+module.exports = {
+  costToSteem,
 };
-
-module.exports = utils;
