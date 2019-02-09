2816c5c82b04ce9c1d85e90dbdd3beb360387d9a
hightouch <hightouch@hotmail.fr>
2019-02-09 23:09:41 +0100

    test rc

--- src/battles/second_round_handler.js
+++ src/battles/second_round_handler.js
@@ -62,9 +62,10 @@ const second_round_handler = {
     continueBattle:function(attacker, defender, cb) {
         var rc = {}
         var aunits = attacker
+        console.log(aunits)
         var cunits = defender
         rc.start ={attacker_units: aunits,defender:cunits}
-        rc[i] = {}
+        rc.rounds=[]
         for (i = 1; i < 6; i++) {
             var round_attackers = second_round_handler.chooseNextAttackersByPriority(aunits)
             var round_defenders = second_round_handler.chooseNextDefAttackersByPriority(cunits)
@@ -112,7 +113,7 @@ const second_round_handler = {
                                 return el != null;
                             })
                         }
-                        rc[i].round = ({ attacker :{attacker: round_attackers.id,start_amount:round_attackers.start_amount, damage: round_attackers.damage, pv:round_attackers.pv, amount: round_attackers.amount}, 
+                        rc.rounds.push({ attacker :{attacker: round_attackers.id,start_amount:round_attackers.start_amount, damage: round_attackers.damage, pv:round_attackers.pv, amount: round_attackers.amount}, 
                         defender : { defender: round_defenders.id,start_amount:round_defenders.start_amount, damage: round_defenders.damage, pv:round_defenders.pv, amount: round_defenders.amount }})  
                         
                     }
