e9d111205b1c587b9d608412f2c62d8a6ea01efb
hightouch <hightouch@hotmail.fr>
2019-02-10 02:11:04 +0100

    added jaws

--- helpers/db.js
+++ helpers/db.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql');
 
+var prod = true 
+if(prod)
 const db = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USERNAME,
@@ -7,6 +9,15 @@ const db = mysql.createPool({
   database: process.env.MYSQL_DB,
   connectionLimit: 50,
   multipleStatements: true,
-});
+})
+else
+const db = mysql.createPool({
+  host: process.env.PROD_MYSQL_HOST,
+  user: process.env.PROD_MYSQL_USERNAME,
+  password: process.env.PROD_MYSQL_PASSWORD,
+  database: process.env.PROD_MYSQL_DB,
+  connectionLimit: 50,
+  multipleStatements: true,
+})
 
 module.exports = db;
