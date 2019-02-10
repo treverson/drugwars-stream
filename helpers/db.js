577aafbe9f7ba0fd602452ec3f28fd561022872d
hightouch <hightouch@hotmail.fr>
2019-02-10 02:13:25 +0100

    miss

--- helpers/db.js
+++ helpers/db.js
@@ -1,16 +1,5 @@
 const mysql = require('mysql');
 
-var prod = true 
-if(prod)
-const db = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
-  connectionLimit: 50,
-  multipleStatements: true,
-})
-else
 const db = mysql.createPool({
   host: process.env.PROD_MYSQL_HOST,
   user: process.env.PROD_MYSQL_USERNAME,
